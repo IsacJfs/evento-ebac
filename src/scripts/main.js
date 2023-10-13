a1f20f48daa43316a962324d29e203c9957d5259
IsacJfs <isac.jfs@gmail.com>
2023-10-13 13:46:12 -0300

    :bookmark: criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const timeStamoDoEvento = dataDoEvento.getTime();
+
+const contaTempo = setInterval(function () {
+    const agora = new Date();
+    const timeStamAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStamoDoEvento - timeStamAtual;
+
+
+    const diaEmMs = 1000 * 60 * 60 * 24
+    const horaEmMs = 1000 * 60 * 60
+    const minutoEmMs = 1000 * 60
+    const segundoEmMs = 1000
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / segundoEmMs);
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaTempo);
+        document.getElementById('contador').innerHTML = 'Evento expirado!'
+    }
+}, 1000)
